c25f43b73f4b07b5eddc7e931ea21e0eb709a694
JeanFrancis <eugenedidit8@gmail.com>
2026-05-02 08:05:05 -0500

    Delete app directory

--- app/thicc-fitt/page.js
+++
@@ -1,6 +0,0 @@
-import '../../styles/sections/thicc-fitt.css';
-import ThiccFittSection from '../../components/sections/ThiccFittSection';
-
-export default function ThiccFittPage() {
-  return <ThiccFittSection />;
-}
