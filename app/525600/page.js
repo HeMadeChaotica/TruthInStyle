aa58c7021d5de6f8577ce13b7215eece7b8c39af
JeanFrancis <eugenedidit8@gmail.com>
2026-06-14 18:39:13 -0500

    Wire approved background assets

--- app/525600/page.js
+++ app/525600/page.js
@@ -1,1 +1,5 @@
-// Temporary scaffold file for local implementation.
+import Annual525600Section from '../../components/sections/Annual525600Section';
+
+export default function Annual525600Page() {
+  return <Annual525600Section />;
+}
--- app/hopewood/page.js
+++ app/hopewood/page.js
@@ -1,1 +1,5 @@
-// Temporary scaffold file for local implementation.
+import HopewoodSection from '../../components/sections/HopewoodSection';
+
+export default function HopewoodPage() {
+  return <HopewoodSection />;
+}
